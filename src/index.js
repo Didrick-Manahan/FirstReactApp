fe26ec3c8d2da09e7c013d65726df74b445fb08d
Didrick Manahan <didrick.manahan@gmail.com>
2022-12-22 02:21:48 -0500

    add comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"; //only call once in entire React application
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//ReactDOM used to render componenets/entire application into the real DOM (i.e. div with id="root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
