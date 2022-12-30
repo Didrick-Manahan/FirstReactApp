15b137dbb0e3caed48ce55e53d83982d8ddc4dfe
Didrick Manahan <didrick.manahan@gmail.com>
2022-12-30 08:03:54 -0500

    Person component dynamic rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const Person = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Name: {props.name ? props.name : "No Name"} </h1>
+      <h2>Age: {props.age}</h2>
+    </>
   );
-}
+};
+
+const App = () => {
+  const name = "Banksy";
+  const isNameShowing = false;
+
+  return (
+    //code below is JSX (class in HTML, className in JS)
+    //inside curly braces, you can put an valid Javascript expression!
+    //You can dynamically render real data inside browser
+    <div className="App"></div>
+
+    /* Person component dyanmic rendering*/
+    //</div><h1> Hello, {name} </h1>
+    //</div><Person name={"John"} age={22 + 22} />
+    //</div><Person name={"Banksy"} age={24} />
+    //</div><Person />
+    //</div>{isNameShowing ? (
+    //</div>  name
+    //</div>) : (
+    //</div>  <>
+    //</div>    <h1>testing failed</h1> <h2>Oh no!</h2>
+    //</div>  </>
+    //</div>)}
+  );
+};
 
 export default App;
